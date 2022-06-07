29df5c57445a7ab8b3122ff24eafecf3c752968f
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2022-06-07 09:05:35 -0400

    restart only

--- training/learner.py
+++ training/learner.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     )
 
     # alg.load("C:/Users/kchin/code/Kaiyotech/abad/checkpoint_save_directory/Coyote_1650839805.8645337/Coyote_240/checkpoint.pt")
-    alg.load("checkpoint_save_directory/Coyote_1654516864.881261/Coyote_2199/checkpoint.pt")
+    alg.load("checkpoint_save_directory/Coyote_1654579277.2051845/Coyote_2233/checkpoint.pt")
     alg.agent.optimizer.param_groups[0]["lr"] = logger.config.learning_rate_actor
     alg.agent.optimizer.param_groups[1]["lr"] = logger.config.learning_rate_critic
 
